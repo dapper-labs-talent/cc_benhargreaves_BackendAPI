743090431dcc70dcb60103c590ebcd48e2faea76
Ben <bhargreaves91@gmail.com>
2022-07-14 16:34:44 -0400

    Add docker files

---
+++ docker_postgres_init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+
+INSERT INTO users(email, password, firstName, lastName)
+VALUES ('Jade@email.com','abc123','Jade','Smith');
+
